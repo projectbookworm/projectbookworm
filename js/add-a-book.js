0f6b224ac1924d0da9fe8eb65a96cadc2c516dcb
river-ceanne <snvencer@gmail.com>
2019-04-16 11:40:47 -0700

    add-a-book js made and some storage  checks in app.js added

--- js/add-a-book.js
+++ js/add-a-book.js
@@ -1,2 +1,36 @@
 'use strict';
 
+//basic variables needed for this page->js
+var saveSubmit = document.getElementById('submit');
+
+
+function addABook(event){
+
+  event.preventDefault();
+
+  let bookTitle = document.getElementById('bookTitle').value;
+  console.log(bookTitle);
+  let author = document.getElementById('author').value;
+  console.log(author);
+  let isbn13 = document.getElementById('isbn13').value;
+  console.log(isbn13);
+  let imageURL = document.getElementById('imageURL').value;
+  console.log(imageURL);
+  let description = document.getElementById('description').value;
+  console.log(description);
+
+  new Book(bookTitle,author,isbn13,imageURL,description);
+  setLocalStorageData();
+  console.log('Library Table:');
+  console.table(library);
+  console.log('LocalStorage Books:');
+  console.table(localStorage.getItem('books'));
+
+  getLocalStorageData();
+  console.log('LocalStorage Books in Library Array:');
+  console.table(library);
+
+}
+
+
+saveSubmit.addEventListener('click',addABook);
