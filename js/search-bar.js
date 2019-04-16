4c1a9b19bb0b431b26006634c35eac0344d8e9b3
Jhia Turner <jhimitu@gmail.com>
2019-04-16 16:26:47 -0700

    get user search input

--- js/search-bar.js
+++ js/search-bar.js
@@ -1,3 +1,27 @@
-function searchBar(keyword){
+'use strict';
+
+var searchSubmit = document.getElementById('search');
+
+searchSubmit.addEventListener('keyup', handleEnterSubmit);
+
+function handleEnterSubmit(e) {
+  if (e.keyCode === 13) {
+    event.preventDefault();
+    document.getElementById('search').click();
+    let query = searchSubmit.value;
+    searchBar(query);
+  }
+}
+
+function searchBar(query){
   //TODO: implement search for nav menu
+  //get books from local storage
+  //for each book
+    //for each porperty on book
+      //companre query with property value
+      //if match
+        //push book to results array
+  //for each result
+    //append result item to dom list
+  console.log(query);
  }
